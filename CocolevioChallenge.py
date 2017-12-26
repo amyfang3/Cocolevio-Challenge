2620882630d1c1228718fc98d6fd1132286e0b2e
Amy Fang <amyfang@utexas.edu>
2017-12-25 19:46:07 -0600

    added findAllCombos and maxProfit functions

--- CocolevioChallenge.py
+++ CocolevioChallenge.py
@@ -1,21 +1,24 @@
+import itertools
+
+# stores a company's info
 class Company():
 
 	def __init__(self, name, amount, price):
 		self.name = name
 		self.amount = amount
 		self.price = price
+		self.profit = amount * price
 
 	def __str__(self):
 		string = "Company " + self.name
-		string += "\nAmount: " + self.amount
-		string += "\nPrice: " + self.price
 		return string
 
 # consolidating data about companies into a list of objects from a csv file
-def upload(datafile):
+def upload():
 	with open("datafile.csv", "r") as f:
 		data = f.readlines()
 
+	# converts each row of data into a list
 	for i in range(len(data)):
 		data[i] = data[i].split(',')
 		data[i][-1] = data[i][-1].replace("\n", "")
@@ -29,17 +32,52 @@ def upload(datafile):
 
 	# adds each company to the list
 	for i in range(num_of_companies):
-		list_all_companies.append(Company(data[0][i+1], data[1][i+1], data[2][i+1]))
+		list_all_companies.append(Company(data[0][i+1], int(data[1][i+1]), int(data[2][i+1])))
 
 	return list_all_companies
 
+# finds all possible permutations of companies
+def findAllCombos(list_all_companies):
+	all_combos = []
+	for size in range(1, len(list_all_companies) + 1):
+		combos = itertools.combinations(list_all_companies, size)
+		for combo in combos:
+			all_combos.append(combo)
+
+	return all_combos
+
+# calculates the highest profit and returns a list of company names
+def maxProfit(total_amount_material, list_all_companiesl):
+	all_combos = findAllCombos(list_all_companies)
+	max_combo = 0
+	max_profit = 0
+
+	for combo in combos:
+		total_amount = 0
+		total_profit = 0
+
+		for company in combo:
+			total_amount += company.amount
+			total_profit += company.profit
+
+		if total_amount > total_amount_material:
+			pass
+		
+		if total_profit > max_profit:
+			max_profit = total_profit
+			max_combo = combo
+
+	return max_combo
 
 
 
 
+def main():
+	list_all_companies = upload()
+	totalAmountMaterial = eval(input("How much material is there? "))
+	print(findAllCombos(list_all_companies))
+	#print(maximizeProfits(totalAmountMaterial, list_all_companies))
+
+main()
 
 
-
-
-
-def function(totalAmountMaterial, data):
